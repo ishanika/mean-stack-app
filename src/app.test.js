e60f2df870e0d66fe491f567992b5842e4d2f91a
Siva Kumar <siva@akikodesign.com>
2022-04-03 19:52:22 +0100

    LogRocket add

--- src/app.test.js
+++ src/app.test.js
@@ -1,11 +1,14 @@
 const app = require("./app")
 const supertest = require("supertest")
 const request = supertest(app)
+import LogRocket from 'logrocket';
+
 
 describe("/test endpoint", () => {
     it("should return a response", async () => {
         const response = await request.get("/test")
         expect(response.status).toBe(200)
         expect(response.text).toBe("Hello world");
+        LogRocket.init('jvc8tx/pokerstars');
     })
 })
